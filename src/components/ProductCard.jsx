a617a6407300093dc30229622aa2eafb4a8a593d
Gulnar <shafigullinagulnaralmazovns@gmail.com>
2024-05-17 18:20:40 +0300

    Done app

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./ProductCard.module.css";
+const ProductCard = ({ title, category, desc, image, price, count }) => {
+  return (
+    <div className={classes.body}>
+      <div className={classes.card}>
+        <div className={classes.title}>{title}</div>
+        <img src={image} className={classes.photo} alt="" />
+        <div className={classes.text}>{desc}</div>
+        <div className={classes.price}>Цена: {price}</div>
+        <div className={classes.price}>В наличии: {count}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
